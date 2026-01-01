8168e5062d8443d2b48a62411e51d74185643d1d
Edu Muller <eduardoaum2007@gmail.com>
2026-01-01 11:55:59 -0300

    Optimized solution two sums

--- arrays_and_hashing/Two_sums.ts
+++ arrays_and_hashing/Two_sums.ts
@@ -3,26 +3,40 @@
  * LINK: https://neetcode.io/problems/two-integer-sum
  * -------------------------------------------------------------------------
  * APPROACH EVOLUTION:
- * 1. Brute Force: O(n^2) - Nested loops
+ * 1. Brute Force: O(n^2) - Nested loops (Function: twoSum)
+ * 2. Optimized: O(n) - Hash Map one pass (Funciont: twoSum2)
  * -------------------------------------------------------------------------
- * TIME COMPLEXITY:  O(n^2) - Single pass each array number
- * SPACE COMPLEXITY: O(1) -
+ * TIME COMPLEXITY:  O(n) - Constant time loop with Map
+ * SPACE COMPLEXITY: O(n) - Storing up to N elements in the Map 
  * PERFORMANCE: Beats 74.55% in Runtime
  * -------------------------------------------------------------------------
  */
-class Solution {
-    /**
-     * @param {number[]} nums
-     * @param {number} target
-     * @return {number[]}
-     */
-    twoSum(nums: [], target: number) {
-        for(let i = 0;i < nums.length;i++){
-            for(let j = i + 1;j < nums.length;j++){
-                if(nums[i] + nums[j] === target){
-                    return [i, j];
-                }
+
+function twoSum(nums: number[], target: number): number[] {
+    for(let i = 0;i < nums.length;i++){
+        for(let j = i + 1;j < nums.length;j++){
+            if(nums[i] + nums[j] === target){
+                return [i, j];
             }
         }
     }
+    return [];
+};
+console.log(twoSum([2,7,11,15], 9));
+
+function twoSum2(nums: number[], target: number): number[] {
+    const prevMap = new Map<number, number>();
+
+    for(let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const completeNum = target - currentNum;
+
+        if(prevMap.has(completeNum)){
+            return [prevMap.get(completeNum)!, i];
+        }
+
+        prevMap.set(currentNum, i);
+    }
+    return [];
 }
+console.log(twoSum2([2,7,11,15], 9));
