28a93da436a0c750b142ff6502fbbbbf927797b0
Edu Muller <eduardoaum2007@gmail.com>
2026-01-02 20:50:12 -0300

    Contains duplicate struct

---
+++ arrays_and_hashing/Contains_duplicate.ts
@@ -0,0 +1,15 @@
+/*
+ * PROBLEM: Contains Duplicate
+ * LINK: https://neetcode.io/problems/duplicate-integer
+ * -------------------------------------------------------------------------
+ * APPROACH EVOLUTION:
+ * 1. Brute Force: O(n^2) -
+ * -------------------------------------------------------------------------
+ * TIME COMPLEXITY:  O(n) - 
+ * SPACE COMPLEXITY: O(n) - 
+ * PERFORMANCE:
+ * -------------------------------------------------------------------------
+ */
+function containsDuplicate(nums: number[]): boolean {
+    return true;
+};
