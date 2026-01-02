7d365c8dd592afa6bca7ed9f74272ff37c9c28d6
Edu Muller <eduardoaum2007@gmail.com>
2026-01-02 20:57:22 -0300

    contains duplicate brute force

--- arrays_and_hashing/Contains_duplicate.ts
+++ arrays_and_hashing/Contains_duplicate.ts
@@ -11,5 +11,12 @@
  * -------------------------------------------------------------------------
  */
 function containsDuplicate(nums: number[]): boolean {
-    return true;
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1;j < nums.length;j++){
+            if(nums[i] === nums[j]){
+                return true;
+            }
+        }
+    }
+    return false;
 };
