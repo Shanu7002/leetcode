55158d7b46c9d7c92c4501ec268b7f11476b3b0f
Edu Muller <eduardoaum2007@gmail.com>
2026-01-10 01:39:20 -0300

    Group anagrams go comments

---
+++ arrays_and_hashing/Group_anagrams.go
@@ -0,0 +1,16 @@
+/*
+ * PROBLEM: Group Anagrams
+ * LINK: https://neetcode.io/problems/anagram-groups
+ * -------------------------------------------------------------------------
+ * APPROACH EVOLUTION:
+ * 1. Frequency Counter: Count character frequencies (26 letters) & use the count as a unique key.
+ * -------------------------------------------------------------------------
+ * TIME COMPLEXITY: O(n * k)
+ * -------------------------------------------------------------------------
+ * SPACE COMPLEXITY: O(1) extra space for the frequency array
+ * -------------------------------------------------------------------------
+ */
+
+ package main
+
+ func anagramGroups()
