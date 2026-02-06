c1b695b8aa60fb465335a327f772df50f27398c3
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 12:42:41 -0300

    feat: TwoSum skeleton

---
+++ Java/array/TwoSum.java
@@ -0,0 +1,18 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] findSumIndices(int[] nums, int target) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] result = findSumIndices(new int[] { 3, 1, 5, 7, 5, 9 }, 10);
+        if (result != null) {
+            System.out.println(result[0] + " " + result[1]);
+        }
+    }
+}
