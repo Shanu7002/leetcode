3158687d1db40afc398e810333547ae3a6f88c24
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 11:35:03 -0300

    feat: StringCompression skeleton

---
+++ Java/array/StringCompression.java
@@ -0,0 +1,15 @@
+package array;
+
+public class StringCompression {
+
+    public static String compress(String str) {
+        // TODO: Retorne a menor entre a original e a comprimida
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compress("aaabb"));
+        System.out.println(compress("abc"));
+        System.out.println(compress("aabcccccaaa"));
+    }
+}
