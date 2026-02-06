8838a4b07dff55c619529f2af49de6904a57a355
Edu Muller <eduardoaum2007@gmail.com>
2026-02-05 23:05:57 -0300

    feat: PalindromeCheck done

--- Java/array/PalindromeCheck.java
+++ Java/array/PalindromeCheck.java
@@ -3,13 +3,20 @@ package array;
 public class PalindromeCheck {
 
     public static boolean isPalindrome(String word) {
-        // TODO: Use dois ponteiros (left e right) para comparar os caracteres
-        // Dica: word.toLowerCase() ajuda na comparação
-        return false;
+        if (word == null || word.isEmpty()) return false;
+
+        String newWord = word.toLowerCase();
+
+        for (int i = 0; i < newWord.length() /  2; i++){
+            if (newWord.charAt(i) != newWord.charAt(newWord.length() - (i + 1))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Deleveled")); // Esperado: true
-        System.out.println(isPalindrome("Axur"));      // Esperado: false
+        System.out.println(isPalindrome("Deleveled"));
+        System.out.println(isPalindrome("Axur"));
     }
 }
