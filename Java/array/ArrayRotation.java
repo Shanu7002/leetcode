3fc040d7ae2bffa4e0ee0ab620ca363873e46419
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 20:33:12 -0300

    feat: ArrayRotation skeleton

---
+++ Java/array/ArrayRotation.java
@@ -0,0 +1,15 @@
+package array;
+
+public class ArrayRotation {
+    public static void rotate(int[] nums, int k) {
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        rotate(nums, 2);
+        for (int i : nums) System.out.print(i + " ");
+    }
+}
