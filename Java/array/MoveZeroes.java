a4205ae52a7fef548f052e62bf17ac1610938128
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 18:10:38 -0300

    feat: MoveZeroes done

--- Java/array/MoveZeroes.java
+++ Java/array/MoveZeroes.java
@@ -2,14 +2,23 @@ package array;
 
 public class MoveZeroes {
     public static void moveZeroes(int[] nums) {
-        // TODO: Use um ponteiro 'lastNonZeroFoundAt' para rastrear onde
-        // colocar o próximo número diferente de zero.
+        int lastNomZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[lastNomZeroFoundAt] = nums[i];
+                lastNomZeroFoundAt++;
+            }
+        }
+
+        for (int i = lastNomZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 3, 12};
         moveZeroes(nums);
-        // Esperado: {1, 3, 12, 0, 0}
         for (int n : nums) System.out.print(n + " ");
+        System.out.println(); // just for remove the %, IDE things, apparently.
     }
 }
