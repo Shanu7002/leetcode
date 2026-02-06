917b907bdf2edae0e9414547cd59febd33e1356a
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 15:15:13 -0300

    feat: TwoSum done

--- Java/array/TwoSum.java
+++ Java/array/TwoSum.java
@@ -5,6 +5,19 @@ import java.util.Map;
 
 public class TwoSum {
     public static int[] findSumIndices(int[] nums, int target) {
+        // value, index
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int actualNumber = nums[i];
+            int actualTarget = target - actualNumber;
+
+            if (map.containsKey(actualTarget)) {
+                return new int[]{ map.get(actualTarget), i };
+            }
+
+            map.put(actualNumber, i);
+        }
 
         return null;
     }
