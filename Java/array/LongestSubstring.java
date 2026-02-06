4fb327621e24e1c238b8eecb1ae5682b6a1a30fe
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 16:06:49 -0300

    feat: LogestSubstring skeleton

---
+++ Java/array/LongestSubstring.java
@@ -0,0 +1,16 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        // TODO: Use um HashSet para controlar os caracteres na janela atual
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); // 3 ("abc")
+        System.out.println(lengthOfLongestSubstring("bbbbb"));    // 1 ("b")
+    }
+}
