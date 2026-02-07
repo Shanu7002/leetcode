b757e10c48d25724b00db037334ac26d19242e06
Edu Muller <eduardoaum2007@gmail.com>
2026-02-06 22:30:09 -0300

    feat: ArrayRotation done

--- Java/array/ArrayRotation.java
+++ Java/array/ArrayRotation.java
@@ -2,14 +2,26 @@ package array;
 
 public class ArrayRotation {
     public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 
     private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        rotate(nums, 2);
+        rotate(nums, 11);
         for (int i : nums) System.out.print(i + " ");
+        System.out.println("\n"); // need this for IDE things, removing the % at final
     }
 }
