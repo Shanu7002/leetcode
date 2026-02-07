8b1a1fa973346fd59a47b15bbd13e19cf697dbf7
Edu Muller <eduardoaum2007@gmail.com>
2026-02-07 11:32:52 -0300

    feat: FirstUnique skeleton

---
+++ Java/array/FirstUnique.java
@@ -0,0 +1,13 @@
+package array;
+
+public class FirstUnique {
+    public static int firstUniqChar(String s) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+        System.out.println(firstUniqChar("loveleetcode"));
+        System.out.println(firstUniqChar("aabb"));
+    }
+}
