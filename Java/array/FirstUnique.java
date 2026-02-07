01257581a034673d738d37b7d9a18f48e51ebca3
Edu Muller <eduardoaum2007@gmail.com>
2026-02-07 11:51:31 -0300

    feat: FirstUnique done

--- Java/array/FirstUnique.java
+++ Java/array/FirstUnique.java
@@ -2,12 +2,31 @@ package array;
 
 public class FirstUnique {
     public static int firstUniqChar(String s) {
+        // fast return
+        if (s == null || s.isEmpty()) return -1;
+
+        // take care with upperCase letters
+        String sLowerCase = s.toLowerCase();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < sLowerCase.length(); i++) {
+            count[sLowerCase.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < sLowerCase.length(); i++) {
+            int actualLetterIndex = sLowerCase.charAt(i) - 'a';
+            if (count[actualLetterIndex] == 1) {
+                return i;
+            }
+        }
+
         return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(firstUniqChar("leetcode"));
-        System.out.println(firstUniqChar("loveleetcode"));
-        System.out.println(firstUniqChar("aabb"));
+        System.out.println(firstUniqChar("leetcode"));     // 0 (o 'l')
+        System.out.println(firstUniqChar("loveleetcode")); // 2 (o 'v')
+        System.out.println(firstUniqChar("aabb"));         // -1
     }
 }
