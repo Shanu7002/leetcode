a43e6f5b044d39b31edcf2e6e0ff468c655b74f5
Edu Muller <eduardoaum2007@gmail.com>
2026-02-08 20:54:44 -0300

    feat: test beecrowd skeleton

---
+++ Java/array/test.java
@@ -0,0 +1,33 @@
+package array;
+
+public class test {
+
+    public static int calculateRequiredWalls(int qnt, int wallSize, String titans, int pSize, int mSize, int gSize) {
+        if (qnt != titans.length()) {
+            throw new IllegalArgumentException("Quantity smaller than titans length.");
+        }
+        if (wallSize <= 0) {
+            throw new IllegalArgumentException("Wall size must be positive.");
+        }
+
+        long totalSize = 0;
+
+        for (int i = 0; i < titans.length(); i++) {
+            char titan = titans.charAt(i);
+            switch (titan) {
+                case 'P' -> totalSize += pSize;
+                case 'M' -> totalSize += mSize;
+                case 'G' -> totalSize += gSize;
+                default  -> { }
+            }
+        }
+
+        return (int) (totalSize / wallSize + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateRequiredWalls(3,20, "MPG", 3, 8, 10));      // Output: 1
+        System.out.println(calculateRequiredWalls(8,20, "MGGPGGGP", 3, 8, 10)); // Output: 2
+        System.out.println(calculateRequiredWalls(4,6, "GPMP", 3, 4, 5));       // Output: 2
+    }
+}
