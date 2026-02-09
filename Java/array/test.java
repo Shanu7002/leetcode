3d4bea6f268cfdcd416ef94e8c5643b7e410713a
Edu Muller <eduardoaum2007@gmail.com>
2026-02-08 21:03:36 -0300

    feat: test beecrowd done

--- Java/array/test.java
+++ Java/array/test.java
@@ -3,31 +3,35 @@ package array;
 public class test {
 
     public static int calculateRequiredWalls(int qnt, int wallSize, String titans, int pSize, int mSize, int gSize) {
-        if (qnt != titans.length()) {
-            throw new IllegalArgumentException("Quantity smaller than titans length.");
-        }
-        if (wallSize <= 0) {
-            throw new IllegalArgumentException("Wall size must be positive.");
-        }
+        // verificação com throw error
+        if (titans == null || titans.isEmpty()) throw new IllegalArgumentException("Titans cannot be empty.");
+        if (qnt != titans.length()) throw new IllegalArgumentException("Quantity smaller than titans length.");
+        if (wallSize <= 0) throw new IllegalArgumentException("Wall size must be positive.");
 
+        // inicializa a resposta final
         long totalSize = 0;
 
+        // percorre cada letra dos titans
         for (int i = 0; i < titans.length(); i++) {
             char titan = titans.charAt(i);
             switch (titan) {
                 case 'P' -> totalSize += pSize;
                 case 'M' -> totalSize += mSize;
                 case 'G' -> totalSize += gSize;
-                default  -> { }
+                // Default case gera erro por motivos obvios
+                default  -> throw new IllegalArgumentException("Unknown titan type: " + titan);
             }
         }
 
-        return (int) (totalSize / wallSize + 1);
+        if (totalSize == 0) return 0;
+
+        // divide o tamanho total pelo tamanho da wall arredondando pra cima
+        return (int) ((totalSize + wallSize - 1) / wallSize);
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateRequiredWalls(3,20, "MPG", 3, 8, 10));      // Output: 1
-        System.out.println(calculateRequiredWalls(8,20, "MGGPGGGP", 3, 8, 10)); // Output: 2
-        System.out.println(calculateRequiredWalls(4,6, "GPMP", 3, 4, 5));       // Output: 2
+        System.out.println(calculateRequiredWalls(3,20, "MPG", 3, 8, 10));
+        System.out.println(calculateRequiredWalls(8,20, "MGGPGGGP", 3, 8, 10));
+        System.out.println(calculateRequiredWalls(4,6, "GPMP", 3, 4, 5));
     }
 }
