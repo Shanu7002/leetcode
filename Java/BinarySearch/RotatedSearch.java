bfe76c32245b851ca1285591620e7373b3dd283d
Edu Muller <eduardoaum2007@gmail.com>
2026-02-10 17:51:29 -0300

    feat: RotatedSearch skeleton

---
+++ Java/BinarySearch/RotatedSearch.java
@@ -0,0 +1,14 @@
+package BinarySearch;
+
+public class RotatedSearch {
+    public static int search(int[] nums, int target) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(nums, 0)); // 4
+        System.out.println(search(nums, 3)); // -1
+    }
+}
