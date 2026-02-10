c93a6ac0d11b97497f5eff3871a0915d7923f354
Edu Muller <eduardoaum2007@gmail.com>
2026-02-10 17:47:46 -0300

    feat: InsetPosition done

--- Java/BinarySearch/InsertPosition.java
+++ Java/BinarySearch/InsertPosition.java
@@ -2,15 +2,31 @@ package BinarySearch;
 
 public class InsertPosition {
     public static int searchInsert(int[] nums, int target) {
+        if (nums == null ) return 0;
 
-        return -1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6};
-        System.out.println(searchInsert(nums, 5));
-        System.out.println(searchInsert(nums, 2));
-        System.out.println(searchInsert(nums, 7));
-        System.out.println(searchInsert(nums, 0));
+        System.out.println(searchInsert(nums, 5)); // 2
+        System.out.println(searchInsert(nums, 2)); // 1
+        System.out.println(searchInsert(nums, 7)); // 4
+        System.out.println(searchInsert(nums, 0)); // 0
     }
 }
