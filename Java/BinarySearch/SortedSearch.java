93539fad7e6664feee98567d86fb37aea9ae36c9
Edu Muller <eduardoaum2007@gmail.com>
2026-02-10 17:20:41 -0300

    feat: SortedSearch done

--- Java/BinarySearch/SortedSearch.java
+++ Java/BinarySearch/SortedSearch.java
@@ -7,9 +7,17 @@ public class SortedSearch {
         int left = 0;
         int right = sortedArray.length - 1;
 
-        while (left <= right) {
+        while (right >= left) {
             int mid = left + (right - left) / 2;
 
+            if (sortedArray[mid] == target) {
+                return mid;
+            }
+            if (sortedArray[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
         return -1;
@@ -17,7 +25,12 @@ public class SortedSearch {
 
     public static void main(String[] args) {
         int[] sortedArray = { 1, 3, 5, 7, 9, 11 };
-        System.out.println(findIndex(sortedArray, 3)); // Esperado: 1
-        System.out.println(findIndex(sortedArray, 8)); // Esperado: -1
+        System.out.println(findIndex(sortedArray, 1));
+        System.out.println(findIndex(sortedArray, 3));
+        System.out.println(findIndex(sortedArray, 5));
+        System.out.println(findIndex(sortedArray, 7));
+        System.out.println(findIndex(sortedArray, 9));
+        System.out.println(findIndex(sortedArray, 11));
+        System.out.println(findIndex(sortedArray, 8));
     }
 }
