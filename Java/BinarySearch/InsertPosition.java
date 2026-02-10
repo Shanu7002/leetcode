c1739ba59614e72e9085d800ca5c7eb35eb6dd31
Edu Muller <eduardoaum2007@gmail.com>
2026-02-10 17:21:45 -0300

    feat: InsetPosition skeleton

---
+++ Java/BinarySearch/InsertPosition.java
@@ -0,0 +1,16 @@
+package BinarySearch;
+
+public class InsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        System.out.println(searchInsert(nums, 5));
+        System.out.println(searchInsert(nums, 2));
+        System.out.println(searchInsert(nums, 7));
+        System.out.println(searchInsert(nums, 0));
+    }
+}
